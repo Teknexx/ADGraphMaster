00685620c21722251991a9b17d447f38f884f1b5
TekneX <84870892+Teknexx@users.noreply.github.com>
2022-05-12 21:39:01 +0200

    Update ADGraphMaster.py

--- ADGraphMaster.py
+++ ADGraphMaster.py
@@ -19,23 +19,23 @@ def HELP():
 
     print("""
     
-       _   ___   ___               _    __  __         _           
-      /_\ |   \ / __|_ _ __ _ _ __| |_ |  \/  |__ _ __| |_ ___ _ _ 
-     / _ \| |) | (_ | '_/ _` | '_ \ ' \| |\/| / _` (_-<  _/ -_) '_|
-    /_/ \_\___/ \___|_| \__,_| .__/_||_|_|  |_\__,_/__/\__\___|_|  
-                             |_|                                     v1.4
-    arguments:  
-      -u : users file (from AD Audit Master)
-      -c : computers file (from AD Audit Master)
-      -b : enable physics buttons
-      -n : name or path of the output file (default : ADGraphMasterCarto.html)
-      -f : find a particular CN
+   _   ___   ___               _    __  __         _           
+  /_\ |   \ / __|_ _ __ _ _ __| |_ |  \/  |__ _ __| |_ ___ _ _ 
+ / _ \| |) | (_ | '_/ _` | '_ \ ' \| |\/| / _` (_-<  _/ -_) '_|
+/_/ \_\___/ \___|_| \__,_| .__/_||_|_|  |_\__,_/__/\__\___|_|  
+                         |_|                                     v1.4
+arguments:  
+  -u : users file (from AD Audit Master)
+  -c : computers file (from AD Audit Master)
+  -b : enable physics buttons
+  -n : name or path of the output file (default : ADGraphMasterCarto.html)
+  -f : find a particular CN
 
-    examples :
-      python3 ADGraphMaster.py -c DC=domain-Computers.csv -u DC=domain-Users.csv -n Carto/CartoExample.html
-      ./ADGraphMaster.py -u DC=domain-Users.csv -b -n HTMLUsers
-      py ADGraphMaster.py -u DC=domain-Users.csv -f 'Jean MICHEL'
-      
+examples :
+  python3 ADGraphMaster.py -c DC=domain-Computers.csv -u DC=domain-Users.csv -n Carto/CartoExample.html
+  ./ADGraphMaster.py -u DC=domain-Users.csv -b -n HTMLUsers
+  py ADGraphMaster.py -u DC=domain-Users.csv -f 'Jean MICHEL'
+  
     """)
     exit()
     
